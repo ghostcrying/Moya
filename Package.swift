bd6b512c8364d7f371851cd8025ba0dd6189d783
ghost <czios1501@gmail.com>
2023-09-20 10:17:02 +0800

    feat: add combine;

--- Package.swift
+++ Package.swift
@@ -9,11 +9,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Moya",
-            targets: ["Moya"]),
+            targets: ["Moya"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0"))
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -21,12 +22,13 @@ let package = Package(
         .target(
             name: "Moya",
             dependencies: [
-                .product(name: "Alamofire", package: "Alamofire")
+                .product(name: "Alamofire", package: "Alamofire"),
             ],
             path: "Sources"
         ),
         .testTarget(
             name: "MoyaTests",
-            dependencies: ["Moya"]),
+            dependencies: ["Moya"]
+        ),
     ]
 )
